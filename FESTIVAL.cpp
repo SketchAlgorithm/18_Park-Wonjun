a77ab9eb57c3c8b431afb2e70a79d91df1087cc0
SketchAlgorithm <47266632+SketchAlgorithm@users.noreply.github.com>
2019-07-01 11:38:34 +0900

    Update FESTIVAL.cpp

--- FESTIVAL.cpp
+++ FESTIVAL.cpp
@@ -1,12 +1,6 @@
 /*
 *	Quest URL : https://algospot.com/judge/problem/read/FESTIVAL
 *	박원준
-*	log : 1) Problem TestCase 1 Wrong 
-			최소비용부터 사용하고, 연속된 일 수가 L을 넘을 시 값 구하는 방법 사용(Greedy)
-			해당 TestCase에서 cost 3을 사용할 때, 전체 비용을 사용하는 경우 발생
-		: 2) Wrong
-			i는 Index가 아닌 상수이므로, 탐색 범위에 포함 해야한다.
-			부등호 등호 처리.
 */
 #include <algorithm>
 #include <iostream>
@@ -31,10 +25,7 @@ int main(void)
 			cin >> costs[i];
 
 		// Print Answer
-		//cout << Festival() << endl;
 		printf("%.12lf\n", Festival());
-
-		// Clear Memory
 	}
 
 	return 0;
